bfe6481d6947b474e98d3332d939724c5efc47c0
Ketan Lambat <ketan.lambat@ymail.com>
2020-04-13 12:12:14 +0530

    folder split

---
+++ src/3x3 Tic Tac Toe/setup.py
@@ -0,0 +1,15 @@
+import cx_Freeze
+
+# name of the main game file
+executables = [cx_Freeze.Executable("TicTacToe_AI.py")]
+
+cx_Freeze.setup(
+    # title of the application window
+    name="Tic Tac Toe AI",
+    options={"build_exe": {"packages": ["pygame"],
+                           # other files that are part of the project
+                           "include_files": ["O.png", "X.png", "AI_algo.py"]}},
+
+    executables=executables
+
+)
---
+++ src/Open Field Tic Tac Toe/setup.py
@@ -0,0 +1,15 @@
+import cx_Freeze
+
+# name of the main game file
+executables = [cx_Freeze.Executable("OpenField_TTT.py")]
+
+cx_Freeze.setup(
+    # title of the application window
+    name="Open Field Tic Tac Toe with AI",
+    options={"build_exe": {"packages": ["pygame"],
+                           # other files that are part of the project
+                           "include_files": ["O.png", "X.png", "AI_algo_custom.py"]}},
+
+    executables=executables
+
+)
